c889a81dc62fedc40afad402a3f3fdfdb94be66d
Maxim Kuzmenko <mway2914@gmail.com>
2022-03-20 19:59:26 +0300

    add property and field to ArrayList class

--- MyFirstStepInClass/ArrayList/ArrayList.cs
+++ MyFirstStepInClass/ArrayList/ArrayList.cs
@@ -4,6 +4,14 @@ namespace ArrayList
 {
     public class ArrayList
     {
+        public int Length { get; private set; }
 
+        private int[] _array;
+
+        public ArrayList()
+        {
+            _array = new int[10];
+            Length = 0;
+        }
     }
 }
